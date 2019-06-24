183321eddd4e8897725cef9b92954791cec17e08
wilantgomari <wilantgomari@gmail.com>
2019-06-23 23:40:35 -0500

    Anális de evaluaciones mediante el modelo de Rasch

---
+++ modelo_rasch.R
@@ -0,0 +1,80 @@
+#Instalar paquetes
+install.packages('TAM')
+install.packages('tidyverse')
+
+#Cargar librería
+library(TAM)
+library(tidyverse)
+
+
+#Cargar los archivos de evaluaciones de los Campus A,B y C
+#Para los diferentes ciclos 2017-2018 (2018) y 2018-2019 (2019)
+campus_A_2018 <- read.csv("/home/wilantgomari/Dropbox/Trabajos/Git/knotion/Modelo de Rasch/prueba1/results/campus_A_2018.csv",row.names=1)
+campus_B_2018 <- read.csv("/home/wilantgomari/Dropbox/Trabajos/Git/knotion/Modelo de Rasch/prueba1/results/campus_B_2018.csv",row.names=1)
+campus_C_2018 <- read.csv("/home/wilantgomari/Dropbox/Trabajos/Git/knotion/Modelo de Rasch/prueba1/results/campus_C_2018.csv",row.names=1)
+campus_A_2019 <- read.csv("/home/wilantgomari/Dropbox/Trabajos/Git/knotion/Modelo de Rasch/prueba1/results/campus_A_2019.csv",row.names=1)
+campus_B_2019 <- read.csv("/home/wilantgomari/Dropbox/Trabajos/Git/knotion/Modelo de Rasch/prueba1/results/campus_B_2019.csv",row.names=1)
+campus_C_2019 <- read.csv("/home/wilantgomari/Dropbox/Trabajos/Git/knotion/Modelo de Rasch/prueba1/results/campus_C_2019.csv",row.names=1)
+
+
+#Unir las evaluaciones del 2018 y 2019 
+datos2018 <- rbind(campus_A_2018,campus_B_2018,campus_C_2018)
+datos2019 <- rbind(campus_A_2019,campus_B_2019,campus_C_2019)
+
+
+#Aplicar modelo de Rasch para dos parámetros en las evaluaciones de 2018 y 2019
+rasch2018 <- TAM::tam.mml.2pl(datos2018,irtmodel="2PL")
+rasch2019 <- TAM::tam.mml.2pl(datos2019,irtmodel="2PL")
+
+#Selecionar las variables AXsi_.Cat1 y B.Cat1.Dim1 que son las variables 
+#de dificultad y descriminación del item respectivamente, y las renombramos
+item2018 <- rasch2018$item %>% select(AXsi_.Cat1, B.Cat1.Dim1)%>% rename(Dificultad=AXsi_.Cat1, Discriminación=B.Cat1.Dim1)
+item2019 <- rasch2019$item %>% select(AXsi_.Cat1, B.Cat1.Dim1)%>% rename(Dificultad=AXsi_.Cat1, Discriminación=B.Cat1.Dim1)
+
+
+#Habilidad de los estudiantes para el 2018 y 2019 
+#Utilizar la funcion tam.wle() (ver documentación del package TAM)
+habilidad2018 <- tam.wle(rasch2018)
+habilidad2019 <- tam.wle(rasch2019)
+
+n1<-nrow(campus_A_2018)
+n2<-n1+nrow(campus_B_2018)
+n3<-n2+nrow(campus_C_2018)
+
+m1<-nrow(campus_A_2019)
+m2<-m1+nrow(campus_B_2019)
+m3<-m2+nrow(campus_C_2019)
+
+
+#Rankin de los alumnos por campus 
+
+
+#ciclo 2018
+habilidad_campus_A_2018 <- habilidad2018 %>% select(theta)%>% rename(habilidad=theta) %>% slice(1:n1)
+habilidad_campus_B_2018 <- habilidad2018 %>% select(theta)%>% rename(habilidad=theta) %>% slice((n1+1):n2)                                                                                                                                            
+habilidad_campus_C_2018 <- habilidad2018 %>% select(theta)%>% rename(habilidad=theta) %>% slice((n2+1):n3)                                                                                                                                            
+#ciclo 2019
+habilidad_campus_A_2019 <- habilidad2019 %>% select(theta)%>% rename(habilidad=theta) %>% slice(1:m1)
+habilidad_campus_B_2019 <- habilidad2019 %>% select(theta)%>% rename(habilidad=theta) %>% slice((m1+1):m2)                                                                                                                                            
+habilidad_campus_C_2019 <- habilidad2019 %>% select(theta)%>% rename(habilidad=theta) %>% slice((m2+1):m3)     
+
+
+
+#Rankin de las escuelas 2018
+nivel_campus_A_2018 <- habilidad_campus_A_2018 %>% summarise(mean = mean(habilidad))
+nivel_campus_B_2018 <- habilidad_campus_B_2018 %>% summarise(mean = mean(habilidad))
+nivel_campus_C_2018 <- habilidad_campus_C_2018 %>% summarise(mean = mean(habilidad))
+
+
+#Rankin de las escuelas 2019
+nivel_campus_A_2019 <- habilidad_campus_A_2019 %>% summarise(mean = mean(habilidad))
+nivel_campus_B_2019 <- habilidad_campus_B_2019 %>% summarise(mean = mean(habilidad))
+nivel_campus_C_2019 <- habilidad_campus_C_2019 %>% summarise(mean = mean(habilidad))
+
+
+
+
+
+
+
+
